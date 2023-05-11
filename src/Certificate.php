aa6f4ae0fb2c5e690a3d67093f9d5bc21841de61
Julio <almiror.info@gmail.com>
2023-05-10 21:27:28 -0500

    �

--- src/Certificate.php
+++ src/Certificate.php
@@ -41,6 +41,7 @@ class Certificate
             // Establecer la posición del cursor en el centro del documento
             $pdf->SetXY($x, $y);
             // Escribir el texto centrado
+            // $pdf->SetFont(TIPO DE FUENTE, GROSOR FUENTE, TAMAÑO DE FUENTE);
             $pdf->SetFont('helvetica', 'XB', 26);
             $pdf->Cell(0, 10, strtoupper($userName), 0, 1, 'C');
 
